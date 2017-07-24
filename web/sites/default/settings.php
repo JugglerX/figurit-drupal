db5a705fa93d4a6884164bae2705a93b13f414e1
Robert Austin <rob@Roberts-MacBook-Pro.local>
2017-07-23 17:53:34 -0700

    update settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -26,9 +26,11 @@ $config_directories = array(
 /**
  * If there is a local settings file, then include it
  */
-$local_settings = __DIR__ . "/settings.local.php";
-if (file_exists($local_settings)) {
-  include $local_settings;
+if (!defined('PANTHEON_ENVIRONMENT')) {
+  $local_settings = __DIR__ . "/settings.local.php";
+  if (file_exists($local_settings)) {
+    include $local_settings;
+  }
 }
 
 /**
