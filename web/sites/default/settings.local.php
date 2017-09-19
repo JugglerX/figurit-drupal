bedbca2c8c3b7818969f01baeaa2da0807ac245e
Robert Austin <rob@jugglerdigital.com>
2017-09-19 12:10:26 +0700

    update settings

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -3,10 +3,14 @@
 assert_options(ASSERT_ACTIVE, TRUE);
 \Drupal\Component\Assertion\Handle::register();
 
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/local.services.yml';
+/**
+ * services.yml
+ */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/services.local.yml';
+
 $config['system.logging']['error_level'] = 'verbose';
-//$config['system.performance']['css']['preprocess'] = FALSE;
-//$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['extension_discovery_scan_tests'] = FALSE;
