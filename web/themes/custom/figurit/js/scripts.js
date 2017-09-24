4e0e83a76033319b6ba4fbbad398165c4e08070f
Robert Austin <rob@jugglerdigital.com>
2017-09-24 21:07:17 +0700

    responsive mobile menu

--- web/themes/custom/figurit/js/scripts.js
+++ web/themes/custom/figurit/js/scripts.js
@@ -2,6 +2,22 @@ console.log('Scripts.js');
 
 (function ($, Drupal) {
 
+  Drupal.behaviors.hamburger = {
+    attach: function (context) {
+      $('#hamburger').on('click', function (e) {
+        console.log($(this));
+        console.log($(this).hasClass('open'));
+        if ($(this).hasClass('open')) {
+          $(this).removeClass('open');
+          $('#block-mainnavigation').removeClass('open');
+        } else {
+          $(this).addClass('open');
+          $('#block-mainnavigation').addClass('open');
+        }
+      });
+    }
+  };
+
   // Drupal.behaviors.tooltipster = {
   //   attach: function (context) {
   //     $('.tooltipster-text').tooltipster({
